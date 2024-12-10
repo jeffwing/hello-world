557f19456132ae020ad562533dabc30108c10b01
jeffwing <jeffwing@hotmail.com>
2024-12-10 17:21:00 +0800

    [admin(管理员) on 20241210092058]

---
+++ src/views/abc/abc_01.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {
+  VueForm, VueFormItem, VueInput,
+  useI18n,
+} from 'viy-ui';
+const { t } = useI18n();
+defineOptions({
+  name: 'abc_01',
+});
+const form = ref();
+const viy2Textarea_KWLH6 = ref();
+const formData = reactive({
+  datafieldviy2Textarea_KWLH6: '',
+});
+</script>
+
+<template>
+  <VueForm ref="form" :model="formData">
+    <VueFormItem
+      label="lkj"
+      prop="datafieldviy2Textarea_KWLH6"
+    >
+      <VueInput
+        id="viy2Textarea_KWLH6"
+        ref="viy2Textarea_KWLH6"
+        v-model="formData.datafieldviy2Textarea_KWLH6"
+      />
+    </VueFormItem>
+  </VueForm>
+</template>
